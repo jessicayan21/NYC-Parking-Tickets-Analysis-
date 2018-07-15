1a55e6b8c969e5cd81dab011d416da17e9ac0c20
jessicayan21 <40904416+jessicayan21@users.noreply.github.com>
2018-07-15 15:30:37 -0400

    Create dictionary.py

---
+++ dictionary.py
@@ -0,0 +1,17 @@
+'''
+Use CSV module to read the Parking.csv and use nested dictionaries to store each parking ticket as a single dictionary 
+into a large list of dictionaries named tickets.
+'''
+import csv 
+
+# Create list variable 'tickets' that stores dictionaries 
+tickets = []
+# Open CSV file, use for loop to read through CSV file, and create a dictionary associated
+# with values in each row 
+with open('Parking.csv') as parking_csv:
+    reader = csv.DictReader(parking_csv)
+    for row in reader:   
+        d = dict(row)
+        tickets.append(d)
+        
+print(tickets)
